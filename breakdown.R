28e24187cb3225535d6176dc5d4b59a2bf749d6e
ciandotson <94946933+ciandotson@users.noreply.github.com>
2025-05-22 12:49:03 -0500

    Update breakdown.R

--- breakdown.R
+++ breakdown.R
@@ -4,7 +4,7 @@
 library(optparse); packageVersion("optparse")
 option_list <- list(
   make_option("--raw_soil", type = "character", help = "filepath containing the raw, untrimmed reads for the reads generated from the v4 primers (bulk soil, rhizosphere, and some nodule samples)"),
-  make_option("--raw_roots", type = "character", help = "filepath containing the raw, untrimmed reads for the reads generated from the v5-v7 primers( root endosphere and some nodule samples)"),
+  make_option("--raw_root", type = "character", help = "filepath containing the raw, untrimmed reads for the reads generated from the v5-v7 primers( root endosphere and some nodule samples)"),
   make_option("--soil_metadata", type = "character", help = "filepath that contains the Comma Separated Values (csv) file of the soil metadata"),
   make_option("--pheno", type = "character", help = "filepath that contains the Comma Separated Values (csv) file of the phenotype data (biomass and nodule counts)"),
   make_option("--reference", type = "character", help = "filepath that contains the reference database to assign taxonomy to the reads"),
@@ -14,7 +14,7 @@ option_list <- list(
 opt <- parse_args(OptionParser(option_list=option_list))
 
 soil.dir <- opt$raw_soil
-root.dir <- opt$raw_roots
+root.dir <- opt$raw_root
 soil.met <- opt$soil_metadata
 nodnbio <- opt$pheno
 reference <- opt$reference
