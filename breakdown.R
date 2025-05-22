97bcb91d6c5fefeefcbfed1c1ced73acb244b2fd
ciandotson <94946933+ciandotson@users.noreply.github.com>
2025-05-22 12:44:45 -0500

    Update breakdown.R

--- breakdown.R
+++ breakdown.R
@@ -4,10 +4,10 @@
 library(optparse); packageVersion("optparse")
 option_list <- list(
   make_option("--raw_soil", type = "character", help = "filepath containing the raw, untrimmed reads for the reads generated from the v4 primers (bulk soil, rhizosphere, and some nodule samples)"),
+  make_option("--raw_root", type = "character", help = "filepath containing the raw, untrimmed reads for the reads generated from the v5-v7 primers( root endosphere and some nodule samples)"),
   make_option("--soil_metadata", type = "character", help = "filepath that contains the Comma Separated Values (csv) file of the soil metadata"),
   make_option("--pheno", type = "character", help = "filepath that contains the Comma Separated Values (csv) file of the phenotype data (biomass and nodule counts)"),
   make_option("--reference", type = "character", help = "filepath that contains the reference database to assign taxonomy to the reads"),
-  make_option("--raw_root", type = "character", help = "filepath containing the raw, untrimmed reads for the reads generated from the v5-v7 primers( root endosphere and some nodule samples)"),
   make_option("--root_metadata", type = "character", help = "filepath that contains the Comma Separated Values (csv) file of the root metadata"),
   make_option("--zymo", type = "character", help = "filepath to where the reference fasta files are for the ZYMO mock culture"))
 
